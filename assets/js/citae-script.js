4e38e49ed69998f740a1d5f82604ab63ddfc1881
Luan Henrique Gomes da Silva <luan.gomes@academico.ifrn.edu.br>
2019-08-19 09:54:21 -0300

    Tela de carregamento

--- assets/js/citae-script.js
+++ assets/js/citae-script.js
@@ -3,7 +3,7 @@ const divsSecContent = document.querySelectorAll("main .sec-content");
 
 function reviewFade(){
     divsSecContent.forEach( (el) => {
-        if( (el.offsetTop < (window.pageYOffset + ((window.innerHeight * 3)/4)) ) && !(el.classList.contains('showed-content')) ){
+        if( (el.offsetTop < (window.pageYOffset + ((window.innerHeight * 3.5)/4)) ) && !(el.classList.contains('showed-content')) ){
             el.classList.add('showed-content');
         }
     });
@@ -11,3 +11,8 @@ function reviewFade(){
 
 window.addEventListener('load', reviewFade);
 document.addEventListener('scroll', reviewFade);
+
+/*---- ANIMAÇÃO DE CARREGAMENTO DA PÁGINA ----*/
+window.onload = () => {
+    setTimeout(() => { document.body.classList.add("page-loaded") },2000);
+}
