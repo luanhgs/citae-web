3750978e17dda335de97e1961068577b1514b192
Luan Henrique Gomes da Silva <luan.gomes@academico.ifrn.edu.br>
2019-08-26 10:27:42 -0300

    Javascript

--- assets/js/citae-script.js
+++ assets/js/citae-script.js
@@ -2,11 +2,13 @@
 const divsSecContent = document.querySelectorAll("main .sec-content");
 
 function reviewFade(){
-    divsSecContent.forEach( (el) => {
-        if( (el.offsetTop < (window.pageYOffset + ((window.innerHeight * 3.5)/4)) ) && !(el.classList.contains('showed-content')) ){
-            el.classList.add('showed-content');
-        }
-    });
+    if(divsSecContent[divsSecContent.length - 1].classList.contains('showed-content') == false){ /*IF pra executar apenas quando necessário*/
+        divsSecContent.forEach( (el) => {
+            if( (el.offsetTop < (window.pageYOffset + ((window.innerHeight * 3.5)/4)) ) && !(el.classList.contains('showed-content')) ){
+                el.classList.add('showed-content');
+            }
+        });
+    }
 }
 
 window.addEventListener('load', reviewFade);
