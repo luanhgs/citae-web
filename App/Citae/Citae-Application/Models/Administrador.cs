9fcb1ab62f40e44e9c7602c30ef706cb5e41b840
Luan Henrique Gomes da Silva <luan.gomes@academico.ifrn.edu.br>
2019-10-10 11:42:06 -0300

    Cadastro

---
+++ App/Citae/Citae-Application/Models/Administrador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Citae_Application.Models
+{
+    public class Administrador
+    {
+        public int idUsuario { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public byte[] imgPerfil { get; set; }
+        public string CPF { get; set; }
+    }
+}
