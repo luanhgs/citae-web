7d8b6eb8c43fa9229605f4ab4f3673e026f52611
Luan Henrique <luanhgs0111@gmail.com>
2019-11-27 10:02:30 -0300

    Scripts Banco

--- DOCS/Banco de Dados/Script Banco de Dados.sql
+++ DOCS/Banco de Dados/Script Banco de Dados.sql
@@ -3,8 +3,10 @@ CREATE TABLE Usuario (
 	nome VARCHAR(45) NOT NULL,
 	email VARCHAR(40) NOT NULL,
 	senha VARCHAR(20) NOT NULL,
-	imgPerfil IMAGE
+	imgPerfil IMAGE,
+	PRIMARY KEY(idUsuario)
 )
+GO
 
 CREATE TABLE Administrador (
 	idAdm INT IDENTITY,
@@ -12,7 +14,53 @@ CREATE TABLE Administrador (
 	email VARCHAR(40) NOT NULL,
 	senha VARCHAR(20) NOT NULL,
 	imgPerfil IMAGE,
-	CPF VARCHAR(13) NOT NULL
+	CPF VARCHAR(13) NOT NULL,
+	PRIMARY KEY(idAdm)
 )
+GO
 
+CREATE TABLE Ocupacao(
+	idOcupacao INT IDENTITY,
+	nome VARCHAR(20),
+	PRIMARY KEY(idOcupacao)
+)
+GO
+
+CREATE TABLE Autor(
+	idAutor INT IDENTITY,
+	nome VARCHAR(30) NOT NULL,
+	img IMAGE NOT NULL,
+	ocupacao_id INT NOT NULL,
+	FOREIGN KEY(ocupacao_id) REFERENCES Ocupacao(idOcupacao),
+	PRIMARY KEY(idAutor)
+)
+GO
+
+CREATE TABLE Categoria(
+	idCategoria INT IDENTITY,
+	nome VARCHAR(45) NOT NULL,
+	img BINARY(100) NOT NULL,
+	PRIMARY KEY(idCategoria)
+)
+GO
+
+CREATE TABLE Citacao(
+	idCitacao INT IDENTITY,
+	texto VARCHAR(1000) NOT NULL,
+	categoria_id INT NOT NULL,
+	autor_id INT NOT NULL,
+	FOREIGN KEY (categoria_id) REFERENCES Categoria(idCategoria),
+	FOREIGN KEY (autor_id) REFERENCES Autor(idAutor),
+	PRIMARY KEY(idCitacao)
+)
+GO
+
+CREATE TABLE ExemploCitacao(
+	idExemploCitacao INT IDENTITY,
+	texto VARCHAR(1000) NOT NULL,
+	citacao_id INT NOT NULL,
+	FOREIGN KEY (citacao_id) REFERENCES Citacao(idCitacao),
+	PRIMARY KEY(idExemploCitacao)
+)
+GO
 
